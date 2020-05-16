210e3c74ab077ccac234cda47a1316f1782b24ae
Christian Tietze <christian.tietze@gmail.com>
2020-05-16 09:59:32 +0200

    Initial Commit

---
+++ ReSwift-Mario/AppDelegate.swift
@@ -0,0 +1,11 @@
+//  Copyright © 2020 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ ReSwift-Mario/ViewController.swift
@@ -0,0 +1,10 @@
+//  Copyright © 2020 Christian Tietze. All rights reserved. Distributed under the MIT License.
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
